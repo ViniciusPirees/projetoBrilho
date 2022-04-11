5f8c6a1a6bd855a720d03219d3540424be6749f9
FelipeGtr01 <Felipe_Gutierre1@outlook.com>
2022-04-11 19:21:07 -0300

    Terceiro Commit

--- App.js
+++ App.js
@@ -1,33 +1,34 @@
 import React, {useState, useEffect} from 'react';
-import { SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import * as Brightness from 'expo-brightness'; // Import da biblioteca Brightness
 import Slider from '@react-native-community/slider'; // Import da biblioteca Slider
 
+
 const App = () => {
-  const [brightness, setBrightness] = useState(0.50);
+  const [brilho, setBrilho] = useState(0.50);
 
   useEffect(() => {
     (async () => {
       const { status } = await Brightness.requestPermissionsAsync();
       if (status === 'granted') {
-        Brightness.setSystemBrightnessAsync(brightness);
+        Brightness.setSystemBrightnessAsync(brilho);
       }
     })();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Brilho: {parseInt(100*brightness)+'%'}</Text>
+      <Text style={styles.texto}>Brilho: {parseInt(100*brilho)+'%'}</Text>
       <Slider style={styles.slider}
           minimumValue={0}
           maximumValue={1}
           minimumTrackTintColor="#026E81"
           maximumTrackTintColor="#00ABBD"
           thumbTintColor="#FF9933"
-          value={brightness}
-          onValueChange={(brightness) => {
-            setBrightness(brightness);
-            Brightness.setSystemBrightnessAsync(brightness);
+          value={brilho}
+          onValueChange={(brilho) => {
+            setBrilho(brilho);
+            Brightness.setSystemBrightnessAsync(brilho);
           }}
       />
     </View>
@@ -46,7 +47,8 @@ const styles = StyleSheet.create({
   },
   texto: {
     fontSize: 20,
-    fontWeight: 'bold',    
+    fontWeight: 'bold',  
+    color: '#fff'  
   },
   slider: {
     width:250,
